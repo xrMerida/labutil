c4265852c1876ee43b6a7a30e34ea675abcec2b0
Xavier Mérida <259985583+xrMerida@users.noreply.github.com>
2026-08-18 22:31:09 -0600

    chore(cmd_update): Allow force updates on demand

--- setup.sh
+++ setup.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
 cmd_update_() {
+  FORCE=false
+  if [[ $1 == "-f" ]]; then
+    FORCE=true
+  fi
+
   echo "Updating labutil..."
   cd "$SCRIPT_DIR" \
     || echo "setup: internal error: $SCRIPT_DIR does not exist"
-  git pull origin main
+
+  if $FORCE; then
+    git pull origin main -f
+  else
+    git pull origin main
+  fi
+
+  if [[ $? -ne 0 && ! $FORCE ]]; then
+    echo "Use '-f' flag to force update"
+    exit 1
+  fi
   echo "Done!"
 }
 
